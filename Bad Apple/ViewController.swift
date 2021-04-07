217bdb72018a85f635d8bcdbb0b49cc9cd0d6679
Neil Sardesai <neil.95@me.com>
2021-04-07 18:07:01 -0400

    Initial Commit

---
+++ Bad Apple/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Bad Apple
+//
+//  Created by Neil Sardesai on 4/7/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
