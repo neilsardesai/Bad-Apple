85fb5d9575dbbf9c2b6b5f0d45bc4ad175bd1b0b
Neil Sardesai <neil.95@me.com>
2021-04-07 18:21:41 -0400

    lol

--- Bad Apple/AppDelegate.swift
+++ Bad Apple/AppDelegate.swift
@@ -6,21 +6,71 @@
 //
 
 import Cocoa
+import SpriteKit
+import AVFoundation
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    static let videoSize = CGSize(width: 27, height: 20)
+    let statusItem = NSStatusBar.system.statusItem(withLength: videoSize.width)
+    let videoPlayer = AVPlayer(url: Bundle.main.url(forResource: "badapple-alpha", withExtension: "mov")!)
     
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let menu = NSMenu()
+        menu.addItem(withTitle: "Restart", action: #selector(restart), keyEquivalent: "")
+        menu.addItem(withTitle: "Quit", action: #selector(quit), keyEquivalent: "")
+        statusItem.menu = menu
+        
+        let scene = SKScene(size: statusItem.button!.frame.size)
+        scene.backgroundColor = .clear
+        
+        let view = ClickIgnoringView(frame: statusItem.button!.frame)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.allowsTransparency = true
+        view.wantsLayer = true
+        
+        // Technically not supposed to modify the layer like this but yolo
+        view.layer?.cornerRadius = 4
+        view.layer?.cornerCurve = .continuous
+        
+        statusItem.button?.addSubview(view)
+        
+        NSLayoutConstraint.activate([
+            view.centerXAnchor.constraint(equalTo: statusItem.button!.centerXAnchor),
+            view.centerYAnchor.constraint(equalTo: statusItem.button!.centerYAnchor),
+            view.widthAnchor.constraint(equalToConstant: AppDelegate.videoSize.width),
+            view.heightAnchor.constraint(equalToConstant: AppDelegate.videoSize.height)
+        ])
+                
+        view.presentScene(scene)
+        
+        // For some reason it crashes if the video node is the only thing in the scene ಠ_ಠ
+        let randomSprite = SKSpriteNode(color: .clear, size: statusItem.button!.frame.size)
+        scene.addChild(randomSprite)
+                
+        let video = SKVideoNode(avPlayer: videoPlayer)
+        video.position = CGPoint(x: view.frame.midX, y: view.frame.midY)
+        video.size = CGSize(width: view.frame.width, height: view.frame.height)
+        scene.addChild(video)
+        videoPlayer.play()
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    @objc func restart() {
+        videoPlayer.seek(to: .zero)
+        videoPlayer.play()
     }
-
+    
+    @objc func quit() {
+        NSApp.terminate(self)
+    }
 
 }
 
+class ClickIgnoringView: SKView {
+    
+    override func hitTest(_ point: NSPoint) -> NSView? {
+        nil
+    }
+    
+}
